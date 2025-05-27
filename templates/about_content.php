01b8963a3263af13780c0f063d16fd0f2582d4a0
NickSFU <nnick.kosolapovv@gmail.com>
2025-05-27 21:32:49 +0700

    about page added, styles fix

---
+++ templates/about_content.php
@@ -0,0 +1,9 @@
+
+<body> 
+    <div class = about-container>
+        <p>Это фотогалерея создана студентами КИ22-06Б Безпалым Денисом и Косолаповым Никитой в рамках лабораторной работы №12 "Интернет и технологии".</p>
+        <div class ="centered-div">
+            <img class = "about-image" src="about_image.jpg">
+        </div>
+    </div>
+</body>
