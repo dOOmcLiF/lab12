8e5c6020824a18dbb7e0f1f051f212e935c38e13
NickSFU <nnick.kosolapovv@gmail.com>
2025-05-27 21:36:16 +0700

    user font size fix

--- templates/menu.php
+++ templates/menu.php
@@ -13,7 +13,7 @@ $menu_items = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <?php if (!empty($_SESSION['user_id'])): ?>
             <li><a href="?page=upload_image">Загрузить фото</a></li>
             <li><a href="?page=my_gallery">Моя галерея</a></li>
-            <li style="color: white; padding: 10px 16px;"> <?= htmlspecialchars($_SESSION['username']) ?></li>
+            <li style="color: white; padding: 10px 16px;font-size: 20px; "> <?= htmlspecialchars($_SESSION['username']) ?></li>
             <?php if (!empty($_SESSION['user_id']) && $_SESSION['user_id'] == 1): ?>
                 <li><a href="edit_menu.php">Редактировать меню</a></li>
             <?php endif; ?>
