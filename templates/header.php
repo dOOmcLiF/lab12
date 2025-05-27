09cad3724c9b58c932ce329e55c2616fd069108b
NickSFU <nnick.kosolapovv@gmail.com>
2025-05-27 20:52:27 +0700

    styles fix, favicon added

--- templates/header.php
+++ templates/header.php
@@ -2,10 +2,11 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Мое приложение</title>
+    <title>Фотогалерея DN</title>
     <link rel="stylesheet" href="assets/styles.css">
+    <link rel="icon" href="/favicon.png" type="image/x-icon">
 </head>
 <body>
 <header>
-    <h1>Добро пожаловать в мое приложение</h1>
+    <h1>Фотогалерея DN</h1>
 </header>
